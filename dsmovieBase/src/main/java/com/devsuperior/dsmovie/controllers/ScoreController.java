b9662d2b1747fc828f941210f6b5439bb53d978e
DennerOl <dener155@hotmail.com>
2024-02-11 20:03:01 -0300

    customizando doc Api swagger

--- dsmovieBase/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ dsmovieBase/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -12,15 +12,17 @@ import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
 import com.devsuperior.dsmovie.services.ScoreService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/scores")
+@Tag(name = "Score", description = "Controller for Score Movie")
 public class ScoreController {
-	
+
 	@Autowired
 	private ScoreService service;
-	
+
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@PutMapping
 	public ResponseEntity<MovieDTO> saveScore(@Valid @RequestBody ScoreDTO dto) {
